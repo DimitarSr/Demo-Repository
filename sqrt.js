bc8d7dfb9e66a68461e11fd148292f27430b71f1
DimitarSr <dimitarsr2003@abv.bg>
2024-10-17 17:12:27 +0300

    edit logic at sqrt.js

--- sqrt.js
+++ sqrt.js
@@ -1,1 +1,7 @@
-export const squareRoot = x => x ** 0.5;
+export const squareRoot = num => {
+    if (num < 0) {
+        return "Square root of a negative number is not a real number.";
+      } else {
+        return Math.sqrt(number);
+      }
+};
